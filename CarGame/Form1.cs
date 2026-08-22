a29dd3db2264e988a229fcfb559c1be0b4e2ef28
danielcarlmiguell-gif <danielcarlmiguell@gmail.com>
2026-08-22 18:42:25 +0800

    Add player car rendering and selection logic

--- CarGame/Form1.cs
+++ CarGame/Form1.cs
@@ -30,7 +30,9 @@ namespace CarGame
         //ENEMY CARS
         private Image[] enemyCarSprites;
 
-
+        //Player
+        private int playerWidth = 55;
+        private int playerHeight = 90;
         public Form1()
         {
             InitializeComponent();
@@ -141,6 +143,23 @@ namespace CarGame
             }
         }
 
+        private void SelectCar(Point mousePos)
+        {
+            if (!choosingCar)
+                return;
+
+            for (int i = 0; i < carFrames.Length; i++)
+            {
+                if (carFrames[i].Contains(mousePos))
+                {
+                    playerCar = cars[i];
+                    choosingCar = false;
+                    Cursor = Cursors.Default;
+                    Invalidate();
+                    return;
+                }
+            }
+        }
 
         //--------------------------- EVENT HANDLERS ---------------------------
         private void TimerRoad_Tick(object sender, EventArgs e)
@@ -172,7 +191,7 @@ namespace CarGame
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
-
+            SelectCar(e.Location);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -181,6 +200,8 @@ namespace CarGame
 
             if (choosingCar)
                 DrawCarSelection(e.Graphics);
+            else
+                DrawPlayer(e.Graphics);
         }
 
 
@@ -215,7 +236,10 @@ namespace CarGame
 
         }
 
-
+        private void DrawPlayer(Graphics g)
+        {
+            g.DrawImage(playerCar, new Rectangle(170, 400, playerWidth, playerHeight));
+        }
 
     }
 }
